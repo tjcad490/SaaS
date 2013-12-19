2de2f81133b256ac8e07044522da7de5c4032753
cheney <lamost710@126.com>
2013-12-19 20:25:35 +0800

    create project

---
+++ AppPlatform/AppPlatform/Areas/AppStore/AppStoreAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AppPlatform.Areas.AppStore
+{
+    public class AppStoreAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "AppStore";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "AppStore_default",
+                "AppStore/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AppPlatform/AppPlatform/Areas/EnterpriseAdmin/EnterpriseAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AppPlatform.Areas.EnterpriseAdmin
+{
+    public class EnterpriseAdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "EnterpriseAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "EnterpriseAdmin_default",
+                "EnterpriseAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
